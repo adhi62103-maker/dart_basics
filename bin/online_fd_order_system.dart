fe3ed1555be1b870309f22c3baea30d70af305f4
adhi62103-maker <adhi62103@gmail.com>
2025-10-29 09:24:22 +0530

    done

--- bin/online_fd_order_system.dart
+++ bin/online_fd_order_system.dart
@@ -5,40 +5,40 @@ void main() {
   bool ordering = true;
 
   while (ordering) {
-    print('\n--- Menu ---');
-    print('1 → Burger = ₹50');
-    print('2 → Pizza  = ₹120');
-    print('3 → Coffee = ₹40');
-    print('4 → Checkout');
-    stdout.write('Enter your choice (1-4): ');
+    print('--- Menu ---\n1 → Burger = ₹50\n2 → Pizza  = ₹120\n3 → Coffee = ₹40\n4 → Checkout');
+    //or print('\n--- Menu ---');
+    //     print('1 → Burger = ₹50');
+    //     print('2 → Pizza  = ₹120');
+    //     print('3 → Coffee = ₹40');
+    //     print('4 → Checkout');
+    print("enter your choice (1-4):");
     int choice = int.parse(stdin.readLineSync()!);
-
+    //stdout.write('enter your choice (1-4):');
+    //int choice = int.parse(stdin.readLineSync()!);
     switch (choice) {
       case 1:
         stdout.write('Enter quantity of Burger: ');
         int qty = int.parse(stdin.readLineSync()!);
         total = total + (50 * qty);
-        print('Added $qty Burger(s). Subtotal = ₹$total');
+        print('Added $qty Burger(s) Subtotal = ₹$total');
         break;
 
       case 2:
         stdout.write('Enter quantity of Pizza: ');
         int qty = int.parse(stdin.readLineSync()!);
         total = total + (120 * qty);
-        print('Added $qty Pizza(s). Subtotal = ₹$total');
+        print('Added $qty Pizza(s) Subtotal = ₹$total');
         break;
 
       case 3:
         stdout.write('Enter quantity of Coffee: ');
         int qty = int.parse(stdin.readLineSync()!);
         total = total + (40 * qty);
-        print('Added $qty Coffee(s). Subtotal = ₹$total');
+        print('Added $qty Coffee(s) Subtotal = ₹$total');
         break;
 
       case 4:
-        print('\n--- Checkout ---');
-        print('Final Bill Amount: ₹$total');
-        print('Thank you for ordering!');
+        print('--- Checkout ---\nFinal Bill Amount: ₹$total\nThank you for ordering!');
         ordering = false; // stop the loop
         break;
 
